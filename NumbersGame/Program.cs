f51e22eaff3643af9d5cd816836842c899663a55
Simon Ståhl <simon.stahl94@gmail.com>
2023-09-19 13:55:48 +0200

    Added method RunGame()

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -4,20 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Start:
+            Console.WriteLine("Välkommen till Gissa nummret!");
+            RunGame();
+        }
+
+        public static void RunGame()
+        {
+        Start:
             int allowedguess = 5;
             int numguess = 0;
             Random random = new Random();
             int Correctnum = random.Next(1, 20);
-            Console.WriteLine("Gissa nummret!");
+            Console.WriteLine("jag tänker på ett nummer mellan 1 och 20. Du får 5 försök att gissa rätt!");
             bool spela = true;
-        
+
             while (spela == true)
             {
-                Start1:
+            Start1:
                 try
                 {
-                    
+
                     int guess = Convert.ToInt32(Console.ReadLine());
                     numguess++;
                     if (guess == Correctnum)
@@ -52,35 +58,42 @@
                     Console.WriteLine("Vänligen skriv en siffra mellan 1 och 20.");
                     goto Start1;
                 }
-               
-             }
+
+            }
             while (spela == false)
             {
-
-
-                Console.WriteLine("Vill  du spela igen? Skriv Ja eller Nej");
-                string replay = Console.ReadLine().ToLower();
-
-                switch (replay)
+                Start3:
+                try
                 {
-                    case "ja":
-                        Console.Clear();
-                        goto Start;
-                       
-                        
-                    default:
-
-                        Console.WriteLine("Ha en trevlig dag!");
-                        Console.ReadKey();
-                        Environment.Exit(0);
-                        break;
-                }
-                
                     
-                
-                
+                    Console.WriteLine("Vill  du spela igen? Skriv Ja eller Nej");
+                    string replay = Console.ReadLine().ToLower();
+
+
+                    switch (replay)
+                    {
+                        case "ja":
+                            Console.Clear();
+                            goto Start;
+
+
+                        default:
+
+                            Console.WriteLine("Ha en trevlig dag!");
+                            Console.ReadKey();
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Vänligen skriv Ja eller Nej");
+                    goto Start3;
+                }
+
+
+
             }
         }
-
     }
 }
