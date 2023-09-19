7f4c5f61c4ca37c58d62439faf6156b847a2b1f6
Simon Ståhl <simon.stahl94@gmail.com>
2023-09-19 12:30:57 +0200

    Add project files.

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace NumbersGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int allowedguess = 5;
+            int numguess = 0;
+            Random random = new Random();
+            int Correctnum = random.Next(1, 20);
+            Console.WriteLine("Gissa nummret!");
+
+        Start:
+            while (true)
+            {
+                try
+                {
+
+
+                    int guess = Convert.ToInt32(Console.ReadLine());
+                    numguess++;
+                    if (guess == Correctnum)
+                    {
+                        Console.WriteLine("Woho! Du gjorde det!");
+                        break;
+
+                    }
+                    if (guess > Correctnum)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för högt! ");
+
+                    }
+                    if (guess < Correctnum)
+                    {
+                        Console.WriteLine("Tyvärr, du gissade för lågt! ");
+
+                    }
+                    if (numguess == allowedguess)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Tyvärr lyckades du inte gissa talet på fem försök!");
+                        Console.ReadKey();
+                        break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Vänligen skriv en siffra mellan 1 och 20.");
+                    goto Start;
+                }
+            }
+        }
+    }
+}
